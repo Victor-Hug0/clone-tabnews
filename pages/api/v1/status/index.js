7b19c79e9e9a8358399a1d35ef9695d1aea5f268
Victor Hugo <victorhugomourabarreto1@gmail.com>
2026-01-21 21:19:50 -0300

    Melhorando retorno do endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,28 @@
 import database from 'infra/database.js'
 
 export default async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log('result', result);
-  response.status(200).json({ status: 'ok' });
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query('SHOW server_version;');
+  const dbVersion = versionResult.rows[0].server_version;
+
+  const maxConectionsResult = await database.query(`SHOW max_connections;`);
+  const maxConnections = maxConectionsResult.rows[0].max_connections;
+
+  const currentConnectionsResult = await database.query(`
+    SELECT count(*) FROM pg_stat_database WHERE datname = current_database();
+  `);
+
+  const currentConnections = currentConnectionsResult.rows[0].count;
+
+  response.status(200).json({ 
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        db_version: dbVersion,
+        max_connections: parseInt(maxConnections, 10),
+        current_connections: parseInt(currentConnections, 10),
+      }
+    }
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET to /api/v1/status should return 200", async () => {
-    const res = await fetch(`http://localhost:3000/api/v1/status`);
-    expect(res.status).toBe(200);
+    const response = await fetch(`http://localhost:3000/api/v1/status`);
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    expect(responseBody.updated_at).toBeDefined();
+    expect(responseBody.dependencies.database.db_version).toBeDefined();
+    expect(responseBody.dependencies.database.max_connections).toBeDefined();
+    expect(responseBody.dependencies.database.current_connections).toBeDefined();
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at);
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt.toISOString());
+
+    expect(typeof responseBody.dependencies.database.db_version).toBe("string");
+    expect(typeof responseBody.dependencies.database.max_connections).toBe("number");
+    expect(typeof responseBody.dependencies.database.current_connections).toBe("number");
+
+    expect(responseBody.dependencies.database.db_version).toEqual("16.0");
+
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    expect(responseBody.dependencies.database.current_connections).toEqual(1);
 });
