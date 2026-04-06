cf7f9f20973aafc1a4f0fece59dbec6bc3745346
Victor Hugo Moura Barreto <victorbarreto@deso-se.com.br>
2026-04-06 15:35:14 -0300

    adds prettier job again

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,26 +1,26 @@
 import retry from "async-retry";
 
 async function waitForAllServices() {
-    await waitForWebServer();
+  await waitForWebServer();
 
-    async function waitForWebServer() {
-        return retry(fetchStatusPage, {
-            retries: 100,
-            maxTimeout: 1000,
-        });
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
-        async function fetchStatusPage() {
-            const response = await fetch("http://localhost:3000/api/v1/status");
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-            if (response.status !== 200) {
-                throw Error();
-            }
-        }
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
+  }
 }
 
 const orchestrator = {
-    waitForAllServices,
+  waitForAllServices,
 };
 
 export default orchestrator;
