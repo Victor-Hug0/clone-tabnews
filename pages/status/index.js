ea6e6f800ed12bfa5890389eef57976b18ed48db
Victor Hugo Moura Barreto <victorbarreto@deso-se.com.br>
2026-04-16 16:37:58 -0300

    feat: add swr and build initial status page

---
+++ pages/status/index.js
@@ -0,0 +1,65 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+
+
+  return (
+    <>
+      <h1>Status Page</h1>
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000
+  });
+
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+
+  return <div>Última atualização: {updatedAtText}</div>
+}
+
+function DatabaseStatus() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000
+  });
+
+  let databaseStatusText = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusText = (
+      <>
+        <div>
+          Versão: {data.dependencies.database.db_version}
+        </div>
+        <div>
+          Conexões atuais: {data.dependencies.database.current_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      {databaseStatusText}
+    </>
+  )
+}
